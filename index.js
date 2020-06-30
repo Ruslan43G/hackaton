d25031509793580830233335a086331cc383ffed
Metanka <themetanka@gmail.com>
2020-06-30 22:16:51 +0300

    feat/тестовый текст редактор

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-import {constants} from '../scripts/constants.js';
-import Section from '../scripts/constants.js';
+import {
+  initialInfo,
+  post,
+  postText,
+  postTitle,
+  headerTitle,
+  headerImg
+} from './scripts/constants.js';
+import Section from './scripts/constants.js';
+import TextEditor from './scripts/TextEditor.js';
