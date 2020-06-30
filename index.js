c32fee40788a9c6d6157bd187c6e23f71f802ee3
Ruslan Tikhomirov <t.tikhomirof@yandex.ru>
2020-06-30 22:13:00 +0300

    feat: сделать класс Post

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import {constants} from '../scripts/constants.js';
-import Section from '../scripts/constants.js';
+import {initialinfo, headerImg, headerTitle, postTitle, postText} from '../scripts/constants.js';
+import Section from '../scripts/Section.js';
+import Post from './scripts/Post.js'
+
+const testObject = {
+    title: 'Тест',
+    text: 'lorem ipsum что-то там в общем этот текст рыба, lorem ipsum что-то там в общем этот текст рыба, lorem ipsum что-то там в общем этот текст рыба'
+}
+
+const renderer = new Section({renderer: () => {
+    const post = new Post('.template-post', testObject);
+    renderer.addItem(post.generatePost());
+}}, 'content')
+
+renderer.renderItems([testObject]);
