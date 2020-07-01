8c65353d4a2f34109f05146ad9a505db235bf794
Ruslan Tikhomirov <t.tikhomirof@yandex.ru>
2020-07-01 14:08:30 +0300

    fied bug

--- index.js
+++ index.js
@@ -6,29 +6,24 @@ import Post from './scripts/Post.js'
 import Header from './scripts/Header.js';
 import ListServise from './scripts/ListServise.js';
 
-console.log(initialinfo);
-
 const header = new Header(initialinfo[0], {title: '.header__title', img: '.header__img'});
 header.setValues()
 
-
 const renderer = new Section({renderer: (item) => {
+    // если объект содержит ключи тайтл и текст
     if ('title' in item && 'text' in item) {
+        //отрисовывем пост с тайтлом из ключа тайтл
         const title = new Post(
             {post: '.template-title',
             title: '.template-title',
             text: '.template-text'}, '.post__title', item.title,);
-
         renderer.addItem(title.generatePost());
-      
+        //отрисовывем пост с текстом из ключа текст
         const text = new Post(
             {post: '.template-text',
             title: '.template-title',
             text: '.template-text'}, '.post__text', item.text,);
-        renderer.addItem(text.generatePost());   
-        
+        renderer.addItem(text.generatePost());      
     }
-    console.log(item);
 }}, 'content')
-
 renderer.renderItems(initialinfo);
