789db2e263d5e619be8da72d159c5c414d6bc147
Metanka <themetanka@gmail.com>
2020-07-01 14:01:54 +0300

    записывает в localStorage, не ломает браузер

--- index.js
+++ index.js
@@ -11,24 +11,30 @@ console.log(initialinfo);
 const header = new Header(initialinfo[0], {title: '.header__title', img: '.header__img'});
 header.setValues()
 
+const newApi = new LocalStorage('dataInfo');
+const localInfo = new ListServise(newApi);
+console.log(newApi);
 
 const renderer = new Section({renderer: (item) => {
     if ('title' in item && 'text' in item) {
         const title = new Post(
             {post: '.template-title',
             title: '.template-title',
-            text: '.template-text'}, '.post__title', item.title,);
+            text: '.template-text'}, '.post__title', item.title);
 
         renderer.addItem(title.generatePost());
-      
+        if (!localInfo.find(item.id)) {
+        localInfo.create(item);};
+
         const text = new Post(
             {post: '.template-text',
             title: '.template-title',
-            text: '.template-text'}, '.post__text', item.text,);
+            text: '.template-text'}, '.post__text', item.text);
         renderer.addItem(text.generatePost());   
         
     }
     console.log(item);
-}}, 'content')
+}}, 'content');
 
-renderer.renderItems(initialinfo);
+renderer.filterRenderer(initialinfo, newApi.getItem('dataInfo'));
+
